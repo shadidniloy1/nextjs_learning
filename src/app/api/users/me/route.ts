3eaa56357a01acdec29f318fa2e0838f253c07cb
Shadid Hasan <shadidhasan1@gmail.com>
2025-09-13 13:33:13 +0600

    profile added

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,21 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { getDataFromToken } from "@/helper/getDataFromToken";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+connect();
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = getDataFromToken(request);
+    console.log(userId);
+    const user = await User.findOne({ _id: userId }).select("-password");
+    console.log(user);
+    return NextResponse.json({
+      message: "User found",
+      data: user,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import axios from "axios";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, {useState} from "react";
 import toast from "react-hot-toast";
 
 const ProfilePage = () => {
   const router = useRouter();
+  const [user, setUser] = useState("nothing")
   const onLogout = async () => {
     try {
       await axios.get("/api/users/logout");
@@ -17,17 +19,31 @@ const ProfilePage = () => {
       toast.error(error.message);
     }
   };
+
+  const getUserProfile = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setUser(res.data.data._id)
+  }
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
       <h2>It is a profile</h2>
       <hr />
       <h3>Profile page</h3>
+      <h2>{user === "nothing" ? "Nothing" : <Link href={`/profile/${user}`}>{user}</Link>}</h2>
       <hr />
       <button
         onClick={onLogout}
         className="mt-4 p-2 bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
       >
         Logout
+      </button>
+
+      <button
+        onClick={getUserProfile}
+        className="mt-4 p-2 bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
+      >
+        Get User
       </button>
     </div>
   );
